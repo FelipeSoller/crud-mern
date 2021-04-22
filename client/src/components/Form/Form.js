cc64327fb23fb1d7067ff0c93c16c0272e50899c
Felipe Soller <soller@outlook.com>
2021-04-22 18:59:23 -0300

    refactor: Desenvolve toda lógica do formulário

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { createParticipant } from '../../actions/participants';
 
 import useStyles from './styles.js';
 
 const Form = () => {
-    const classes = useStyles();
+    const [participantData, setParticipantData] = useState({
+        firstName: '',
+        lastName: '',
+        participation: '',
+    });
 
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        dispatch(createParticipant(participantData));
+    }
+
+    const classes = useStyles();
+    
     return (
       <div className={classes.header}>
-        <form>            
+        <form autocomplete="off" noValidate onSubmit={handleSubmit}>            
             <input
                 className={classes.input}
-                type="text"
-                placeholder="Nome"        
+                name="firstName" 
+                label="FirstName" 
+                placeholder="Nome"
+                value={participantData.firstName}
+                onChange={(e) => setParticipantData({ ...participantData, firstName: e.target.value })}
             />
             <input
                 className={classes.input}
-                type="text"
-                placeholder="Sobrenome"        
+                name="lastName" 
+                label="LastName" 
+                placeholder="Sobrenome"
+                value={participantData.lastName}
+                onChange={(e) => setParticipantData({ ...participantData, lastName: e.target.value })}
             />
             <input
                 className={classes.input}
-                type="number"
-                placeholder="Participação"            
-            />           
+                name="participation" 
+                label="Participation" 
+                placeholder="Participação"
+                value={participantData.participation}
+                onChange={(e) => setParticipantData({ ...participantData, participation: e.target.value })}
+            />
+                     
             <button className={classes.button} type="submit">ENVIAR</button>
         </form>
       </div>
