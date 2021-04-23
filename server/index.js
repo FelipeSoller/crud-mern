13baa4af6d1e9b0e8d7a86d831f0aa2e0512620e
Felipe Soller <soller@outlook.com>
2021-04-23 15:33:49 -0300

    feat: Cria variáveis de ambiente

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import participantRoutes from './routes/participants.js'
 
 const app = express();
+dotenv.config();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
@@ -12,10 +14,9 @@ app.use(cors());
 
 app.use('/participants', participantRoutes);
 
-const CONNECTION_URL= 'mongodb+srv://crud-mern:crudmern123@cluster0.tnmdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error));
 
